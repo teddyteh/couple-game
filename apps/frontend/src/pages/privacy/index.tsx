5e3f28ca037831f7fefda69f8610d23e9527b47e
Teddy <manwithwrangler@gmail.com>
2023-10-18 23:33:47 +0800

    chroe: add privacy policy

---
+++ apps/frontend/src/pages/privacy/index.tsx
@@ -0,0 +1,69 @@
+const Privacy = () => {
+  return (
+    <div className="main-container">
+      <div className="wrapper">
+        <h1>Privacy Policy</h1>
+        <p>
+          <strong>Last Updated: 18 October 2023</strong>
+        </p>
+
+        <p>
+          Welcome to Couple Game! Your privacy is of utmost importance to us.
+          This Privacy Policy outlines the type of personal information that is
+          not received, collected, and used by Couple Game.
+        </p>
+
+        <h2>1. Information We Don't Collect</h2>
+        <p>
+          We value your privacy and, in line with our commitment to protect your
+          privacy, we do not collect any personal information when you use
+          Couple Game.
+        </p>
+
+        <h2>2. Log Files</h2>
+        <p>
+          As with most other apps, Couple Game does not make use of log files.
+          We don't store any data about your usage of the app, your device, or
+          any other information.
+        </p>
+
+        <h2>3. Cookies and Web Beacons</h2>
+        <p>Couple Game does not use cookies or web beacons.</p>
+
+        <h2>4. Third-Party Advertisers</h2>
+        <p>
+          We do not partner with or have special relationships with any ad
+          server companies.
+        </p>
+
+        <h2>5. Children's Information</h2>
+        <p>
+          Protecting the privacy of young children is especially important to
+          us. For that reason, Couple Game does not knowingly collect or
+          maintain information from those under the age of 13, and no part of
+          our app is structured to attract anyone under 13.
+        </p>
+
+        <h2>6. Consent</h2>
+        <p>
+          By using our app, you hereby consent to our privacy policy and agree
+          to its terms.
+        </p>
+
+        <h2>7. Updates</h2>
+        <p>
+          This Privacy Policy is subject to change and any changes will be
+          posted on this page.
+        </p>
+
+        <h2>8. Contact Us</h2>
+        <p>
+          If you have any questions about this Privacy Policy, please contact
+          us.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Privacy;
