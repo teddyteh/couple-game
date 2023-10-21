cce7cf81cd71eaf0f7fec58ff356c8a74191bee8
Teddy <manwithwrangler@gmail.com>
2023-10-22 01:07:55 +0800

    chore: add retry mechanism

--- apps/frontend/src/utils/advice.ts
+++ apps/frontend/src/utils/advice.ts
@@ -3,34 +3,50 @@ import { Advice } from "@/types/advice";
 const isAdvice = (payload: any): payload is Advice =>
   payload.shortSummary && payload.suggestions;
 
-export const fetchAdvice = async (payload: {
-  answers: {
-    player1Answer: string;
-    player2Answer: string;
-  }[];
-}) => {
-  try {
-    console.info("payload", payload);
+export const fetchAdvice = async (
+  payload: {
+    answers: {
+      player1Answer: string;
+      player2Answer: string;
+    }[];
+  },
+  retries = 2,
+  retryDelay = 500
+) => {
+  let attempts = 0;
 
-    const response = await fetch(
-      "https://zr3k3wtzb0.execute-api.us-east-1.amazonaws.com",
-      {
-        method: "POST",
-        body: JSON.stringify(payload),
-        mode: "cors",
-        cache: "no-store",
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+  while (attempts < retries) {
+    try {
+      console.info("payload", payload);
+
+      const response = await fetch(
+        "https://zr3k3wtzb0.execute-api.us-east-1.amazonaws.com",
+        {
+          method: "POST",
+          body: JSON.stringify(payload),
+          mode: "cors",
+          cache: "no-store",
+        }
+      );
+
+      const advice = <Advice>await response.json();
+      console.info("advice", advice);
+      if (!isAdvice(advice)) {
+        throw new Error("Not quite an advice...");
       }
-    );
 
-    const advice = <Advice>await response.json();
-    console.info("advice", advice);
-    if (!isAdvice(advice)) {
-      throw new Error("Not quite an advice...");
+      return advice;
+    } catch (error) {
+      attempts++;
+      console.error(`Error getting advice (attempt ${attempts}):`, error);
+      if (attempts < retries) {
+        await delay(retryDelay);
+      }
     }
-
-    return advice;
-  } catch (error) {
-    console.error("Error getting advice:", error);
-    return null;
   }
+
+  return null;
 };
